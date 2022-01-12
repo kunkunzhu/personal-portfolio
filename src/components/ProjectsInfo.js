cf8fa126b4f12374d58f34b40d9f607ed13d54fd
kunkunzhu <zkzhu@uwaterloo.ca>
2022-01-12 14:44:42 -0500

    unfinished proj placeholders

--- src/components/ProjectsInfo.js
+++ src/components/ProjectsInfo.js
@@ -10,16 +10,26 @@ const projects = {
         prompt: "behind the scene.",
         path: '/portfolio/technova'
     },
-    brand: {
+    minilib: {
         key: "brand",
-        image: "https://i.ibb.co/XFHdZwW/Mood-Pic-1.png",
-        description: "halo",
+        image: "https://i.ibb.co/5xbzQxt/143667261-d3776d07-2bc1-4861-afe4-3bc197e4076c.png",
+        description: "Recording reading progress on the books you want to read.",
         color: "#D0DEE7",
-        title: "Personal Portfolio",
-        subtitle: "hehe",
-        categories: "UX Design, Frontend Development",
+        title: "Mini Library.",
+        subtitle: "Personal Project",
+        categories: "Web Development - React, JavaScript, MongoDB",
+        prompt: "github.",
+        path: '/portfolio'
+    },
+    mood: {
+        key: "mood",
+        image: "https://i.ibb.co/ZWGWbLq/i-Phone-X-Mockup.png",
+        description: "Cultivating awareness of emotions.",
+        title: "moodment.",
+        subtitle: "Personal Project",
+        categories: "UX Design",
         prompt: "case study.",
-        path: '/portfolio/mood'
+        path: "/portfolio"
     },
     voicenoted: {
         key: "voicenoted",
